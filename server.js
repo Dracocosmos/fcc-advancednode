03be9aa7e363788c6f7349222b0625b1151edd78
dracocosmos <124890142+Dracocosmos@users.noreply.github.com>
2024-11-26 12:17:00 +0200

    started first exercises

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
+// express needs to know this
+app.set('view engine', 'pug');
+app.set('views', "./views/pug")
 
+app.route('/').get((req, res) => {
+  res.render('index', {
+    title: 'Hello',
+    message: 'Please log in'
+  })
 });
 
 const PORT = process.env.PORT || 3000;
